971b0593d5f26af30ac6f7c9cfe2bb62e0a6fc92
Andreas Hagström <ahagstroem@gmail.com>
2020-05-27 19:03:31 +0200

    adding database connection

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const db = knex({
   client: "pg",
   connection: {
     // Heroku
-    host: "127.0.0.1",
-    user: "",
-    password: "",
-    database: "smart-brain",
+    host: "process.env.DATABASE_URL",
+    ssl: "true",
   },
 });
 
