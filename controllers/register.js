219c2e1e67b3869875a3e79007a0692e5f8d14b8
Andreas Hagström <ahagstroem@gmail.com>
2020-06-08 16:15:33 +0200

    bug fix package.json & register.js

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,6 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
+
   if (!email || !name || !password) {
     return res.status(400).json("Nu var det något som blev fel😖");
   }
