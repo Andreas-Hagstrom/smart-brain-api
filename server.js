0cab5e8ff5d975682711d35808d8323f6198bf7d
Andreas Hagström <ahagstroem@gmail.com>
2020-06-09 14:09:34 +0200

    bug fix

--- server.js
+++ server.js
@@ -56,13 +56,9 @@ app.post("/imageurl", (req, res) => {
 //   console.log(hash);
 // });
 
-app.listen(3000, () => {
-  console.log("app is running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
 });
-
-// app.listen(process.env.PORT || 3000, () => {
-//   console.log(`app is running on port ${process.env.PORT}`);
-// });
 
 /* PLANNING OF THE API
 (root-route) /--> res = this is working
