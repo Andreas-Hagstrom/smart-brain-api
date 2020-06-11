4f9f5b5cb1aae02e10d6b988eddb2e6f0c88401d
Andreas Hagström <ahagstroem@gmail.com>
2020-06-11 11:22:09 +0200

    adding db connection

--- server.js
+++ server.js
@@ -12,17 +12,15 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-concave-44725",
-    user: "andreashagstrom",
-    password: "",
-    database: "smart-brain",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
 const app = express();
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 // ROOT
 app.get("/", (req, res) => {
